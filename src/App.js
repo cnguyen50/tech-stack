84da79498c2b2ff90f0043e1c3619c9c8270871b
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-03-12 19:40:40 -0800

    imported header from common folder

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { View } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducers from './reducers'
+import { Header } from './components/common'
 
 const App = () => {
   return (
