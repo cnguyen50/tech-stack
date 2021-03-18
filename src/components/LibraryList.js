25d7fd6e3c42a796bc0cc659959c84543a02b74e
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-03-17 21:25:56 -0700

    key extractor prop to flatList

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -3,11 +3,16 @@ import { FlatList } from 'react-native'
 import { connect } from 'react-redux'
 
 class LibraryList extends Component {
+  renderItem(library) {
+
+  }
+
   render() {
     return (
       <FlatList 
         data={this.props.libraries}
         renderItem={this.renderItem}
+        keyExtractor={(library) => library.id}
       />
     )
   }
