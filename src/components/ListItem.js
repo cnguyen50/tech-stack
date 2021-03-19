cbe166450d339357594e12798617a76a4f0cf8dc
Calvin Nguyen <calvinnguyen50@gmail.com>
2021-03-18 18:48:54 -0700

    imported text and rendered cardSection

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import { CardSection } from './common'
+import { Text } from 'react-native'
 
 class ListItem extends Component {
   render() {
-
+    <CardSection>
+      
+    </CardSection>
   }
 }
 
